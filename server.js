b59020f6f10ff46ba325447955fd7d35b3e02a80
Katie Williams <kawilliams8@gmail.com>
2019-10-01 14:05:15 -0600

    Install cors and add require to server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cors());
 
 app.set("port", process.env.PORT || 3000);
 app.locals.title = "BYOB";
