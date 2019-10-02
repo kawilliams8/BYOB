fe3fb1297a82ad0ff4b8218e2397e87032fdda9c
Katie Williams <kawilliams8@gmail.com>
2019-10-02 14:28:58 -0600

    Install CSV parser package to iterate through CSV, added headers to CSV data

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const csv = require("csv-parser");
+const fs = require("fs");
 app.use(express.json());
 app.use(express.static("public"));
 app.use(cors());
@@ -17,3 +19,21 @@ app.listen(app.get("port"), () => {
     `${app.locals.title} is running on http://localhost:${app.get("port")}.`
   );
 });
+
+const avalanches = [];
+
+fs.createReadStream("avalanches.csv")
+  .pipe(csv())
+  .on("data", data => avalanches.push(data))
+  .on("end", () => {
+    console.log(avalanches);
+  });
+
+const forecast_zones = [];
+
+fs.createReadStream("forecast_zones.csv")
+  .pipe(csv())
+  .on("data", data => forecast_zones.push(data))
+  .on("end", () => {
+    console.log(forecast_zones);
+  });
