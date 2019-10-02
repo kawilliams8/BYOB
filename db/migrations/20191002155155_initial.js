4c1d52fa2fc9d34f5d4ff934773e602ba18c13d2
Katie Williams <kawilliams8@gmail.com>
2019-10-02 17:20:34 -0600

    Complete initial table schema for zones and avys

--- db/migrations/20191002155155_initial.js
+++ db/migrations/20191002155155_initial.js
@@ -1,6 +1,30 @@
 
 exports.up = function(knex) {
-  
+  return Promise.all([
+    knex.schema.createTable("forecast_zones", function(table) {
+      table.increments("id").primary();
+      table.string("zone");
+      table.string("nearby_city");
+      table.string("land_features");
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable("avalanches", function(table) {
+      table.increments("id").primary();
+      table.string("date");
+      table.string("first_name");
+      table.string("last_name");
+      table.string("aspect");
+      table.string("type");
+      table.string("trigger");
+      table.string("elevation");
+      table.integer("zone_id").unsigned();
+      table.foreign("zone_id").references("zone.id");
+
+      table.timestamps(true, true);
+    })
+  ]);
 };
 
 exports.down = function(knex) {
