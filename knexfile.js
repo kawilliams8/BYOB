9b8061786fb73f2876ef843fc08f7f715b5c8098
Katie Williams <kawilliams8@gmail.com>
2019-10-02 15:54:20 -0600

    Begin migrations, run migrate:make initial

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,9 @@ module.exports = {
   development: {
     client: "pg",
     connection: "postgres://localhost/avalanches",
+    migrations: {
+      directory: "./db/migrations"
+    },
     useNullAsDefault: true
   }
 };
