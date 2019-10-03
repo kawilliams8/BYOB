a7037fa33f2d22cf5c16679888cbe90ca93a510c
Katie Williams <kawilliams8@gmail.com>
2019-10-03 13:50:30 -0600

    Update four GETs with error conditional

--- server.js
+++ server.js
@@ -40,34 +40,42 @@ app.get("/api/v1/avalanches/:id", (request, response) => {
     .select()
     .then(avalanches => avalanches.find(avalanche => avalanche.id === id))
     .then(avalanche => {
-      response.status(200).json({ avalanche });
+      if (avalanche === {}) {
+        response.status(200).json({ avalanche });
+      } else {
+        response.status(404).json("Avalanche not found");
+      }
     })
     .catch(error => {
-      response.status(404).json({ error: "Avalanche not found" });
+      response.status(500).json({ error });
     });
 });
 
 app.get("/api/v1/forecast_zones", (request, response) => {
   database("forecast_zones")
-  .select()
+    .select()
     .then(forecast_zones => {
       response.status(200).json(forecast_zones);
     })
     .catch(error => {
-      response.status(404).json({ error });
+      response.status(500).json({ error });
     });
 });
 
 app.get("/api/v1/forecast_zones/:id", (request, response) => {
   const id = parseInt(request.params.id);
-
+  
   database("forecast_zones")
     .select()
     .then(forecast_zones => forecast_zones.find(forecast_zone => forecast_zone.id === id))
     .then(forecast_zone => {
-      response.status(200).json({ forecast_zone });
+      if (forecast_zone === {}) {
+        response.status(200).json({ forecast_zone });
+      } else {
+        response.status(404).json('Forecast zone not found');
+      }
     })
     .catch(error => {
-      response.status(404).json({ error: "Forecast zone not found" });
+      response.status(500).json({ error });
     });
 });
