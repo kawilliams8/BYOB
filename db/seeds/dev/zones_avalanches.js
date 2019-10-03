96122bf1a838b89262136d62645c839da091ce9d
Katie Williams <kawilliams8@gmail.com>
2019-10-02 18:17:49 -0600

    Add helper functions to seed zones and avalanches

--- db/seeds/dev/zones_avalanches.js
+++ db/seeds/dev/zones_avalanches.js
@@ -1,4 +1,43 @@
-import { forecast_zones, avalanches } from '../../../server'; 
+import { forecast_zones, avalanches } from '../../../server';
+
+const createForecastZone = (knex, forecast_zone) => {
+  return knex("forecast_zones")
+    .insert(
+      {
+        zone_name: forecast_zone.zone_name,
+        nearby_city: forecast_zone.nearby_city,
+        land_features: forecast_zone.land_features
+      },
+      "id"
+    )
+    .then(zoneId => {
+      let avalanchePromises = [];
+
+      forecast_zone.avalanches.forEach(avalanche => {
+        avalanchePromises.push(
+          createAvalanche(knex, {
+            date: avalanche.date,
+            date_precision: avalanche.date_precision,
+            first_name: avalanche.first_name,
+            last_name: avalanche.last_name,
+            elevation: avalanche.elevation,
+            aspect: avalanche.aspect,
+            type: avalanche.type,
+            trigger: avalanche.trigger,
+            release_size: avalanche.release_size,
+            destructive_size: avalanche.destructive_size,
+            forecast_zone_id: zoneId[0]
+          })
+        );
+      });
+
+      return Promise.all(avalanchePromises);
+    });
+};
+
+const createAvalanche = (knex, footnote) => {
+  return knex("avalanches").insert(avalanche);
+};
 
 exports.seed = function(knex) {
   return knex('avalanches').del()
