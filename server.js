4340e425d631bc43b07ffeef8845da598f1b218d
Katie Williams <kawilliams8@gmail.com>
2019-10-02 20:40:51 -0600

    Achieve combined data in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const csv = require("csv-parser");
 const fs = require("fs");
+
 app.use(express.json());
 app.use(express.static("public"));
 app.use(cors());
@@ -20,36 +21,51 @@ app.listen(app.get("port"), () => {
   );
 });
 
-const parseAvalanches = () => {
-  const avalancheData = [];
-  fs.createReadStream("avalanches.csv")
-    .pipe(csv())
-    .on("data", data => avalancheData.push(data))
-    .on("end", () => {
-      return avalancheData;
-    });
-}
-
-const parseForecastZones = () => {
-  const forecastZoneData = [];
-  fs.createReadStream("forecast_zones.csv")
-    .pipe(csv())
-    .on("data", data => forecastZoneData.push(data))
-    .on("end", () => {
-      return forecastZoneData;
-    });
-}
-
-const combinedData = async () => {
-  let avalanches = parseAvalanches();
-  let zones = parseForecastZones();
-  return await zones.map(zone => {
-    zone.avalanches = avalanches.filter(
-      avalanche => avalanche.zone_name === zone.zone_name
-    );
-    return zone;
-  })
-  .catch(error => console.log('ERROR!!!', error))
+const parseForecastData = () => {
+  let promise = new Promise((resolve, reject) => {
+    if (true) {
+    const forecastZoneData = [];
+    fs.createReadStream("forecast_zones.csv")
+      .pipe(csv())
+      .on("data", data => forecastZoneData.push(data))
+      .on("end", () => {
+        resolve(forecastZoneData)
+      });
+    } else {
+      reject(Error("Could not parse forecast zone CSV data"));
+    }
+  });
+  return promise;
 };
 
-console.log('trying to combine', combinedData())
+const parseAvalanches = () => {
+  let promise = new Promise((resolve, reject) => {
+    if (true) {
+      const avalancheData = [];
+      fs.createReadStream("avalanches.csv")
+        .pipe(csv())
+        .on("data", data => avalancheData.push(data))
+        .on("end", () => {
+          resolve(avalancheData);
+        });
+    } else {
+      reject(Error("Could not parse avalanche CSV data"));
+    }
+  });
+  return promise;
+};
+
+let combinedData = [];
+parseForecastData()
+  .then(response => combinedData = response)
+  .then(() => parseAvalanches())
+  .then(response => {
+    combinedData.map(zone => {
+      zone.avalanches = response.filter(
+        avalanche => avalanche.zone_name === zone.zone_name
+      );
+      return zone;
+    });
+  })
+  .then(() => console.log(combinedData  ))
+  .catch(err => console.log(err));
