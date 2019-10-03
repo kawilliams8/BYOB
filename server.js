e1700099e49d6263200c8e40334e1d73eb228847
Katherine Williams <44121288+kawilliams8@users.noreply.github.com>
2019-10-03 14:06:17 -0600

    Endpoints (#13)
    * All avalanche data endpoint working
    
    * Add all forecast_zones data endpoint
    
    * Add endpoint to return one avalanche by id
    
    * Add endpoint to return one forecast zone by id
    
    * Update four GETs with error conditional
    
    * Correct logic in GET by id conditional
    
    * Remove conditional logic for errors, return empty object instead

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const environment = process.env.NODE_ENV || "development";
+const configuration = require("./knexfile")[environment];
+const database = require("knex")(configuration);
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -17,4 +20,54 @@ app.listen(app.get("port"), () => {
   console.log(
     `${app.locals.title} is running on http://localhost:${app.get("port")}.`
   );
+});
+
+app.get("/api/v1/avalanches", (request, response) => {
+  database("avalanches")
+    .select()
+    .then(avalanches => {
+      response.status(200).json(avalanches);
+    })
+    .catch(error => {
+      response.status(404).json({ error });
+    });
+});
+
+app.get("/api/v1/avalanches/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+
+  database("avalanches")
+    .select()
+    .then(avalanches => avalanches.find(avalanche => avalanche.id === id))
+    .then(avalanche => {
+        response.status(200).json({ avalanche });
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get("/api/v1/forecast_zones", (request, response) => {
+  database("forecast_zones")
+    .select()
+    .then(forecast_zones => {
+      response.status(200).json(forecast_zones);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get("/api/v1/forecast_zones/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  
+  database("forecast_zones")
+    .select()
+    .then(forecast_zones => forecast_zones.find(forecast_zone => forecast_zone.id === id))
+    .then(forecast_zone => {
+        response.status(200).json({ forecast_zone });
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
 });
