0d247bba0c9d8d423678a166c25bebb2df85d5dd
Katie Williams <kawilliams8@gmail.com>
2019-10-03 10:54:43 -0600

    All avalanche data endpoint working

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const environment = process.env.NODE_ENV || "development";
+const configuration = require("./knexfile")[environment];
+const database = require("knex")(configuration);
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -17,4 +20,15 @@ app.listen(app.get("port"), () => {
   console.log(
     `${app.locals.title} is running on http://localhost:${app.get("port")}.`
   );
+});
+
+app.get("/api/v1/avalanches", (request, response) => {
+  database("avalanches")
+    .select()
+    .then(avalanches => {
+      response.status(200).json(avalanches);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
 });
